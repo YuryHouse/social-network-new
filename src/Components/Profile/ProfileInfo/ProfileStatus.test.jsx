ad986538f2c4261cc1ca0b64f70767fe8ce268bf
YuryHouse <yury.house@yahoo.com>
2022-10-24 12:02:02 +0300

    using react-test-renderer part 3 (mockCallback)

--- src/Components/Profile/ProfileInfo/ProfileStatus.test.jsx
+++ src/Components/Profile/ProfileInfo/ProfileStatus.test.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { create } from "react-test-renderer";
+import {create} from "react-test-renderer";
 import ProfileStatus from "./ProfileStatus";
 
 describe("ProfileStatus component", () => {
@@ -30,5 +30,22 @@ describe("ProfileStatus component", () => {
             root.findByType("input");
         }).toThrow();
     });
-
+    test("input should be displayed in edit mode instead of span", () => {
+        const component = create(<ProfileStatus status={"Hire me! I'm really awesome!"}/>);
+        const root = component.root;
+        // eslint-disable-next-line testing-library/await-async-query
+        let span = root.findByType("span");
+        span.props.onDoubleClick();
+        // eslint-disable-next-line testing-library/await-async-query
+        let input = root.findByType("input");
+        expect(input.props.value).toBe("Hire me! I'm really awesome!");
+    });
+    test("callback should be called", () => {
+        const mockCallback = jest.fn()
+        const component = create(<ProfileStatus status={"Hire me! I'm really awesome!"}
+        updateStatus={mockCallback} />);
+        const instance = component.getInstance();
+        instance.deactivateEditMode();
+        expect(mockCallback.mock.calls.length).toBe(1);
+    });
 });
